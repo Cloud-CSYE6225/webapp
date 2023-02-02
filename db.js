2c956176e3baf9ae17b8b402e234876ff76f959c
saideepak <dogiparthi.sai@northeastern.edu>
2023-02-01 20:31:48 -0500

    Final push

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const {Sequelize} = require('sequelize');
 //   port: process.env.DB_PORT,
 // });
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, 'postgresql', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   port:process.env.DB_PORT,
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,22 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+
+const app = require('../app');
+
+let should = chai.should();
+chai.use(chaiHTTP);
+
+describe('Test of /healthz GET API', () => {
+    it('health should be OK', (done) => {
+        chai.request(app)
+            .get('/healthz')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
 var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
@@ -7,22 +26,6 @@ describe('Array', function () {
   });
 });
 
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(5), -1);
-    });
-  });
-});
 
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(10), -1);
-    });
-  });
-});
 
 
