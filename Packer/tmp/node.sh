c1c9ac13bab6ed1a0091c3840a3367fcb1db29ab
saideepak <dogiparthi.sai@northeastern.edu>
2023-02-22 14:19:40 -0500

    Added packer config files and sh files

---
+++ Packer/tmp/node.sh
@@ -0,0 +1,28 @@
+sleep 15
+
+
+
+sudo yum update -y
+
+
+
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install -y nodejs
+
+
+
+sudo yum install unzip
+unzip /home/ec2-user/webapp.zip
+sudo chmod 755 webapp/
+cd ./webapp
+sudo npm i
+sudo npm i bcrypt
+
+
+sleep 10
+sudo mv /tmp/node.service /etc/systemd/system/node.service
+
+sudo systemctl enable node.service
+
+sudo systemctl start node.service
